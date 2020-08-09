666c505d1ed78ec40301de5b67c7192e4306d903
rohithmone27 <62833958+rohithmone27@users.noreply.github.com>
2020-08-09 21:43:34 +0530

    Added Level 2 Solutions

--- Level 2/Arrays/Wave Array.cpp
+++ Level 2/Arrays/Wave Array.cpp
@@ -2,7 +2,7 @@ vector<int> Solution::wave(vector<int> &A) {
     int n=A.size();
     sort(A.begin(),A.end());
     for(int i=1;i<n;i+=2){
-        swap(A[i],A[i-1]);
+        swap(A[i], A[i-1]);
     }
     return A;
 }
